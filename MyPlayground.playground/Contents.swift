acf8ff6aefc51ae832e917de7dc45eeb25df3985
董超 <1247757314@qq.com>
2022-03-11 09:16:32 +0800

    第二次提交

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,22 +1,17 @@
 import UIKit
 
 class Solution {
-    
-    class Solution {
-        func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-            var i = 0
-            for num in nums{
-                i += 1
-                for index in i...nums.count-1{
-                    if (num + nums[index]) == target{
-                        return [i-1, index]
-                    }
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var i = 0
+        for num in nums{
+            i += 1
+            for index in i...nums.count-1{
+                if (num + nums[index]) == target{
+                    return [i-1, index]
                 }
-
             }
-            return []
-        }
-    }
 
-    
+        }
+        return []
+    }
 }
